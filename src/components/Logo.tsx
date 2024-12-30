509e79e48815a39700e6f2510a0e706e1f9ae1a7
Nhung Nguyen <iminb2b@gmail.com>
2024-12-30 20:37:18 +0700

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -13,6 +13,7 @@ const logo = ({ primary }: { primary: boolean }) =>
       color: primary ? colors.primary : colors.background,
       display: "flex",
       alignItems: "center",
+      gap: "0.5rem",
     },
     {
       "@media screen and (max-width: 1000px)": {
@@ -32,18 +33,23 @@ const Logo: FC<{ variant: "primary" | "secondary" }> = ({ variant }) => {
       css={logo({ primary: variant === "primary" })}
     >
       <svg
-        width="40"
-        height="40"
-        viewBox="0 0 156 116"
+        width="21"
+        height="21"
+        viewBox="0 0 60 60"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect x="26" width="50" height="50" rx="10" fill="#0BE3B7" />
-        <circle cx="106" cy="26" r="25" fill="#E78FB3" />
-        <path
-          d="M84.6409 110.101C80.8529 113.466 75.1471 113.466 71.3591 110.101L30.1257 73.4765C23.2495 67.3689 27.5696 56 36.7666 56L119.233 56C128.43 56 132.75 67.3689 125.874 73.4765L84.6409 110.101Z"
-          fill="#F1C40F"
+        <rect
+          x="5"
+          y="5"
+          width="30"
+          height="30"
+          stroke="currentcolor"
+          stroke-width="10"
         />
+        <path d="M15 40H25V60H15V40Z" fill="currentcolor" />
+        <rect x="25" y="50" width="35" height="10" fill="currentcolor" />
+        <rect x="50" width="10" height="60" fill="currentcolor" />
       </svg>
       min
     </Link>
